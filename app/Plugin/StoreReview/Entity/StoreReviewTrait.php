73ff9f253c84c4076655b1cb35deb2540a1c3813
intern <intern@BraTechMac.local>
2022-02-24 14:57:39 +0900

    task220224v1

---
+++ app/Plugin/StoreReview/Entity/StoreReviewTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\StoreReview\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Eccube\Annotation as Eccube;
+
+/**
+ * @Eccube\EntityExtension("Plugin\ProductReview4\Entity\ProductReview")
+ */
+trait StoreReviewTrait
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="store_review", type="text")
+     */
+    private $store_review;
+
+    /**
+     * Get store_review.
+     *
+     * @return string
+     */
+    public function getStoreReview()
+    {
+        return $this->store_review;
+    }
+
+    /**
+     * Set store_review.
+     *
+     * @param string $store_review
+     *
+     * @return ProductReview
+     */
+    public function setStoreReview($store_review)
+    {
+        $this->store_review = $store_review;
+
+        return $this;
+    }
+}
